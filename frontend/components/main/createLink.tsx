7394008d59b7610ac9b5efa62ccf261e108be6dc
nwakakukaks <winnernwakaku123@gmail.com>
2024-10-14 10:23:10 +0100

    https://aptopus.vercel.app/

--- frontend/components/main/createLink.tsx
+++ frontend/components/main/createLink.tsx
@@ -39,11 +39,9 @@ const CreatorLinkGenerator: React.FC = () => {
     const videoId = extractVideoId(videoUrl);
     if (videoId && aptosAddress) {
       try {
-        const response = await fetch("https://aptopus.vercel.app/generate-short-url", {
+        const response = await fetch("https://aptopus-backend.vercel.app/generate-short-url", {
           method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
+         
           body: JSON.stringify({ videoId, address: aptosAddress }),
         });
         const data = await response.json();
