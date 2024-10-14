f4b8d88bda9cc9d6817b1618babe11dfc5b0cc1c
nwakakukaks <winnernwakaku123@gmail.com>
2024-10-14 17:16:20 +0100

    done

--- frontend/pages/access/page.tsx
+++ frontend/pages/access/page.tsx
@@ -2,7 +2,7 @@ import Access from "@/components/main/access";
 
 export const AccessPage = () => {
   return (
-    <div className="w-full px-4 pt-24">
+    <div className="w-full px-4 pt-24 h-[80vh]">
       <Access />
     </div>
   );
