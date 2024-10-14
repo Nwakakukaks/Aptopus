7d26ff87e92eb7fb8a28796a983c23ca04d8df39
nwakakukaks <winnernwakaku123@gmail.com>
2024-10-14 10:23:51 +0100

    fix

--- frontend/components/main/createLink.tsx
+++ frontend/components/main/createLink.tsx
@@ -41,7 +41,9 @@ const CreatorLinkGenerator: React.FC = () => {
       try {
         const response = await fetch("https://aptopus-backend.vercel.app/generate-short-url", {
           method: "POST",
-         
+          headers: {
+            "Content-Type": "application/json",
+          },
           body: JSON.stringify({ videoId, address: aptosAddress }),
         });
         const data = await response.json();
